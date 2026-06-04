b3375bcf8ccacc2f356c674ad5e89fea585e3e12
ayushsarvaiya31 <ayushsaravaiya@gmail.com>
2026-06-04 19:19:22 +0530

    conform
    conform

--- src/app/sales/lead/page.js
+++ src/app/sales/lead/page.js
@@ -1752,7 +1752,7 @@ export default function Page() {
                           ...updateForm,
                           description: e.target.value,
                         })
-                      }thhhhh
+                      }
                       className="w-full mt-1.5 border border-orange-300 rounded-sm px-3 py-2 text-sm  outline-none bg-gray-50 h-20 resize-none"
                     />
                   </div>
